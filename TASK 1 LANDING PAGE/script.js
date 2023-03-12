ff5a1f3fea1190d64b1cff060faad661d93adf99
adityaprakash01 <adityaprakash72095@gmail.com>
2023-03-12 23:57:42 +0530

    LANDING PAGE

---
+++ TASK 1 LANDING PAGE/script.js
@@ -0,0 +1,23 @@
+// for arrow slider
+const arrows=document.querySelectorAll(".arrow");
+// we use querySelectorAll instead of only querySelector to refernce all 4 arrows class
+const gamelist=document.querySelectorAll(".game-list");
+
+arrows.forEach((arrow,i)=>{
+    const itemNumber=gamelist[i].querySelectorAll("img").length;
+    let clickcounter=0;
+    arrow.addEventListener("click",()=>{
+        const ratio=Math.floor(window.innerWidth/210);
+        // ratio is for mobile view
+        clickcounter++;
+        if(itemNumber-(5+clickcounter)+(6-ratio)>=0){
+            // +(5-ratio) is for mobile view sliding 
+            gamelist[i].style.transform=`translateX(${gamelist[i].computedStyleMap().get("transform")[0].x.value-240}px)`;
+        }else{
+            gamelist[i].style.transform="translateX(0)";
+            clickcounter=0;
+        }
+    });
+    // console.log(Math.floor(window.innerWidth/210));
+    // for mobile view slider
+});
